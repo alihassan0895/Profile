b74a033fbaecaddc735fd115a6bd0ebdf8907dfc
alihassan0895 <108417087+alihassan0895@users.noreply.github.com>
2023-01-14 21:26:35 +0200

    update  style flag lang

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -62,8 +62,9 @@
             <!-- Messages Dropdown Menu -->
             <li class="nav-item dropdown">
                 <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="far fa-comments"></i>
-                    <span class="badge badge-danger navbar-badge">3</span>
+{{--                    <i class="far fa-comments"></i>--}}
+                    Lang
+                    <span class="badge badge-danger navbar-badge">2</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
 
